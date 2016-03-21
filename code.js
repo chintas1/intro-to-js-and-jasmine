a7882c272ca0136b95ec14e17aff48a597936181
chintas1 <chintas1@tcnj.edu>
2016-03-21 15:14:10 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(icecream){
+  return "I love " + icecream;
+}
+
+function shouting(msg){
+  return msg.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(){
+  return true;
+}
